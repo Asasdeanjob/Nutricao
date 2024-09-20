c97c89a2ac9b634468149d3fcf3793feeccf9964
Bryan Daniel de Souza Fabricio <133381833+Asasdeanjob@users.noreply.github.com>
2024-09-20 12:12:04 -0300

    20/09/24

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+var titulo = document.querySelector("h1");
+titulo.textContent = "Dan Nutri";
+console.log(titulo);
+
+var trPaulo = document.querySelector("#paulo");
+console.log(trPaulo);
+
+var tdPeso = trPaulo.querySelector(".info-peso");
+tdPeso.textContent = "150";
+console.log(tdPeso);
