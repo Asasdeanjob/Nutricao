73d202cdfa632932f994d18b42099b5b3bfead84
Bryan Daniel de Souza Fabricio <133381833+Asasdeanjob@users.noreply.github.com>
2024-09-30 10:39:55 -0300

    30/09/24

--- main.js
+++ main.js
@@ -1,37 +1,48 @@
-var titulo          = document.querySelector(".titulo");
+/*Titulo*/
+var titulo            = document.querySelector(".titulo");
+titulo.textContent    = "Dan Nutrição";
+/*Fim Titulo*/
 
-titulo.textContent  = "Dan Nutrição";
-
-var pacientes       = document.querySelectorAll(".paciente");
+/*Pacientes*/
+var pacientes         = document.querySelectorAll(".paciente");
 
 for (var i = 0; i < 5; i++) {
+
   console.log(pacientes[i]);
-  
-  var tdPeso = pacientes[i].querySelector(".info-peso");
-  var peso = tdPeso.textContent;
-  
-  var tdAlto = pacientes[i].querySelector(".info-altura");
-  var alto = tdAlto.textContent;
-  
-  var tdImc = pacientes[i].querySelector(".info-imc");
-  
-  var pesoVal = true;
-  var altoVal = true;
-  
+
+  var tdPeso          = pacientes[i].querySelector(".info-peso");
+  var peso            = tdPeso.textContent;
+
+  var tdAlto          = pacientes[i].querySelector(".info-altura");
+  var alto            = tdAlto.textContent;
+
+  var tdImc           = pacientes[i].querySelector(".info-imc");
+
+  var pesoVal         = true;
+  var altoVal         = true;
+
   if (peso < 0 || peso > 600) {
+
     tdImc.textContent = "peso inválido";
-    pesoVal = false;
+    pesoVal           = false;
+    pacientes[i].style.color = "Red";
+
   }
-  
+
   if (alto < 0 || alto > 2.8) {
+
     tdImc.textContent = "altura inválida"
-    altoVal = false;
+    altoVal           = false;
+
   }
-  
+
   if (pesoVal && altoVal) {
-    var imc = peso / (alto * alto);
+
+    var imc           = peso / (alto * alto);
     tdImc.textContent = imc;
+
   }
 }
 
-var paciente        = document.querySelector("#primeiro-paciente");
+var paciente          = document.querySelector("#primeiro-paciente");
+/*Fim Paciente*/
